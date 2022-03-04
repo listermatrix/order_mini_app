b57324dca912c99011eeedcf00009f27e1c88bf1
Melchizedek <melchizedek@Melchizedeks-MacBook-Pro.local>
2022-03-04 22:37:04 +0000

    updated landing page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard/e_tran/transaction/index';
 
     /**
      * Define your route model bindings, pattern filters, etc.
